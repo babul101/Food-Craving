9e206b23f024d0dcf5bd68ce44546bc991d7b866
babul101 <babuldeb101@gmail.com>
2024-03-16 16:35:32 +0530

    configured smooth scrolling

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const mainTag = document.querySelector("main");
+const bodyTag = document.querySelector("body");
+
+const motion = window.matchMedia("(prefers-reduced-motion:no-preference)");
+const large = window.matchMedia("(min-width:600px)");
+
+if (motion.matches && large.matches) {
+  mainTag.style.position = "fixed";
+
+  mainTag.style.top = "0px";
+
+  mainTag.style.left = "0px";
+
+  mainTag.style.width = "100%";
+
+  let currentScroll = 0;
+  let aimScroll = 0;
+
+  const changeScroll = function () {
+    bodyTag.style.height = mainTag.offsetHeight + "px";
+    currentScroll = currentScroll + (aimScroll - currentScroll) * 0.05;
+    mainTag.style.top = -1 * currentScroll + "px";
+    requestAnimationFrame(changeScroll);
+  };
+
+  window.addEventListener("scroll", function () {
+    aimScroll = this.window.pageYOffset;
+  });
+
+  changeScroll();
+}
